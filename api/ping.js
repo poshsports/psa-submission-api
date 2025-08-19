213fb86d73b7faa47e7033afbaff7c80c6e07bd7
poshsports <dbposhsports@gmail.com>
2025-08-19 17:44:13 -0400

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,7 +1,7 @@
-// api/ping.js
-module.exports = (req, res) => {
+// api/ping.js (ESM)
+export default function handler(req, res) {
   res.status(200).json({ ok: true, msg: 'pong', method: req.method });
-};
+}
 
 // Force Node runtime (not Edge)
-module.exports.config = { runtime: 'nodejs18.x' };
+export const config = { runtime: 'nodejs18.x' };
