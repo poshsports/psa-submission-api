4aa9b60b1f90e491f71a6bd5299555d4620bc348
poshsports <dbposhsports@gmail.com>
2025-09-02 17:39:27 -0400

    Update status.js

--- api/admin/groups/[id]/submissions/status.js
+++ api/admin/groups/[id]/submissions/status.js
@@ -1,6 +1,6 @@
-// /api/admin/groups/[id]/submissions/status.js (ESM)
-import { requireAdmin } from '../../../_util/adminAuth.js';
-import { sb } from '../../../_util/supabase.js';
+// /api/admin/groups/[id]/submissions/status.js
+import { requireAdmin } from '../../../../_util/adminAuth.js';
+import { sb } from '../../../../_util/supabase.js';
 
 const UUID_RE =
   /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
