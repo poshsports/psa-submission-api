72f7ae67cedf56acd8ba8431676ee2b1a24656b5
poshsports <dbposhsports@gmail.com>
2025-09-03 15:16:43 -0400

    Delete pages directory

--- pages/admin/index.tsx
+++
@@ -1,9 +0,0 @@
-// pages/admin/index.tsx
-export async function getServerSideProps() {
-  return {
-    redirect: { destination: '/admin/groups', permanent: false },
-  };
-}
-export default function AdminIndex() {
-  return null;
-}
